8ff8d0085f5190be0d8a9198452c13cb489e670f
out_akopyan <out_akopyan@mkb.ru>
2021-07-05 22:34:22 +0300

    Implemented Button layout

---
+++ linkit-app/src/main/java/com/example/linkit_app/ui/button/ButtonLayout.kt
@@ -0,0 +1,59 @@
+package com.example.linkit_app.ui.button
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.linkit_app.ui.theme.*
+
+@Composable
+fun ButtonLayout(
+    text: String,
+    enabled: Boolean,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        enabled = enabled,
+        shape = RoundedCornerShape(16.dp),
+        elevation = ButtonDefaults.elevation(
+            defaultElevation = 4.dp,
+            pressedElevation = 2.dp,
+            disabledElevation = 2.dp
+        ),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Secondary,
+            disabledBackgroundColor = Disabled
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(52.dp)
+    ) {
+        Text(
+            text = text,
+            color = if (enabled) Black else Grey,
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ButtonLayoutPreview() {
+    LinkitTheme {
+        Box(
+            modifier = Modifier.fillMaxWidth().padding(24.dp)
+        ) {
+            ButtonLayout(
+                text = "Button",
+                enabled = true
+            ) {}
+        }
+    }
+}
